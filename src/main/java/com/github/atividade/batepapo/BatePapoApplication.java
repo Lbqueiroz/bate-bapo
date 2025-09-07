2b6b7e208807ffb777ec7a681da896e7d6e600ba
Lucas <lucasbreniomo@gmail.com>
2025-09-07 18:45:21 -0300

    renomeando pasta

---
+++ src/main/java/com/github/atividade/batepapo/BatePapoApplication.java
@@ -0,0 +1,13 @@
+package com.github.atividade.batepapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatePapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatePapoApplication.class, args);
+	}
+
+}
